f3b4a3f098c3bc147ae3a6b692245ed029fe69fc
lshapz <laurashapiro01@gmail.com>
2017-01-25 13:47:22 -0500

    one_row

---
+++ README.md
@@ -0,0 +1,18 @@
+binary puzzle
+Takuzu
+binoxxo
+tic-tac-logic 
+Binairo
+Tohu wa Vohu
+Eins und Zwei
+Zernero
+
+
+http://www.nearly42.org/vdisk/cstheory/binaryp.pdf
+
+1. Each box should contain a zero or a one.
+2. No more than two similar numbers next to or below each other are allowed.
+3. Each row and each column should contain an equal number of zeros and ones.
+4. Each row is unique and each column is unique.
+
+I am trying to make a generator for this game. Wish me luck! 
---
+++ one_row.rb
@@ -0,0 +1,36 @@
+require 'byebug'
+
+class OneRow
+  attr_accessor :grid, :size
+  
+  def initialize(number)
+    @grid = Array.new(number)
+    @size = number 
+  end
+
+  def generate_row_one   
+    (0..@size).each do |row_one|
+      assign_a_place(row_one)
+    end
+  end
+
+  def assign_a_place(row)
+    if @grid[row-1] == @grid[row-2] 
+      @grid[row] = opposite(@grid[row-1])
+    else
+      @grid[row] = 1 + rand(2)
+    end 
+  end
+
+  def opposite(nearby)
+    if nearby == 1 
+      2
+    else
+      1
+    end
+  end
+
+end
+foo = OnePuzzle.new(16)
+foo.generate_row_one
+p foo.grid
